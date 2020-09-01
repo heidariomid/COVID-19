c214be8aedf86462c9ea6890ea8667590bd7c3a1
Omid Heidari <omid_heidari90@yahoo.com>
2020-09-01 16:13:37 +0430

    cs

---
+++ build/precache-manifest.129034729eb64d566fb041d9efc56105.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b202126a95526b6087d006e5d2f45dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d63a4e0f3683555824b",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "0f411bb6da36f932f185",
+    "url": "/static/css/main.ecac35f0.chunk.css"
+  },
+  {
+    "revision": "0d63a4e0f3683555824b",
+    "url": "/static/js/2.042604fa.chunk.js"
+  },
+  {
+    "revision": "3ad76833de97695745f4fc6932481651",
+    "url": "/static/js/2.042604fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f411bb6da36f932f185",
+    "url": "/static/js/main.c03b4389.chunk.js"
+  },
+  {
+    "revision": "fbeef8cb8629e52ddbd8",
+    "url": "/static/js/runtime-main.f2428489.js"
+  },
+  {
+    "revision": "05a32df97679ab4550b2f61ad49e7832",
+    "url": "/static/media/corona.05a32df9.png"
+  }
+]);
